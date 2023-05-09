32f6ea36c8f307802b715668c6d75a8f774b5eeb
Renan Gimenez <renangiimenez@gmail.com>
2023-05-09 03:16:05 -0300

    edit App

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Card from './components/Card';
-import Div from './components/Div';
 
 import './App.css';
+import Input from './components/Input';
+import CharacterList from './components/CharacterList';
+import Modal from './components/Modal';
 
 function App() {
-
-  const [charactersList, setCharactersList] = useState([]);
-
-  useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-    .then((res) => res.json())
-    .then((data) => {
-      setCharactersList(data.results);
-      console.log(charactersList);
-    })
-  }, [])
+  const [inputValue, setInputValue] = useState('');
   
   return (
     <div className="App">
-      <Header />
-      <h1>Characters:</h1>
+      <Header>
+        <p>Rick and Morty Collection</p>
+        <Input onChange={(e) => {
+          setInputValue(e.target.value);
+          console.log(e.target.value);
+        }} />
+      </Header>
 
-      <Div>
-        {charactersList.map((e) => (
-          <Card 
-            image={e.image} 
-            name={e.name} 
-            status={e.status} 
-            species={e.species}
-            location={e.origin.name} 
-            firstSeen={e.location.name} 
-          />
-        ))}
-      </Div>
+      <h2>Characters:</h2>
 
-      <Footer>
-        <h2>This is a Footer</h2>
-      </Footer>
+      <CharacterList inputValue={inputValue}/>
+
+      {/* <Modal name = {"Renan"}/> */}
+
+      <Footer />
     </div>
   );
 }
